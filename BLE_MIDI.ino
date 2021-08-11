46442d395b5c19f91f4b63e85f64072a5d721d1c
Alexander <saniaxxx@Mac-Pro-Alexander.local>
2021-08-12 00:01:53 +0300

    read shift register

--- BLE_MIDI.ino
+++ BLE_MIDI.ino
@@ -16,6 +16,10 @@
 #define SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
 #define CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
 
+const byte LATCH = 25;
+const byte DATA  = 26;
+const byte CLOCK = 14;
+
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 
@@ -29,19 +33,11 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
-void setup() {
-  Serial.begin(115200);
-
-  BLEDevice::init("ESP32 MIDI");
-
-  // Create the BLE Server
+void setupBle() {
+  BLEDevice::init("MIDI HARMONICA");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
   BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID));
-
-  // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
                       BLEUUID(CHARACTERISTIC_UUID),
                       BLECharacteristic::PROPERTY_READ   |
@@ -49,33 +45,26 @@ void setup() {
                       BLECharacteristic::PROPERTY_NOTIFY |
                       BLECharacteristic::PROPERTY_WRITE_NR
                     );
-
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
   pCharacteristic->addDescriptor(new BLE2902());
-
-  // Start the service
   pService->start();
-
-  // Start advertising
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->addServiceUUID(pService->getUUID());
   pAdvertising->start();
 }
 
-void loop() {
-  if (deviceConnected) {
-    // note down
-    noteOn(pCharacteristic, C5, CHANNEL0);
-    noteOn(pCharacteristic, F5, CHANNEL1);
-    delay(500);
-    // note up
-    noteOff(pCharacteristic, C5, CHANNEL0);
-    noteOff(pCharacteristic, F5, CHANNEL1);
-    delay(500);
-  }
+void setupKeyboard() {
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(DATA, INPUT);
+  digitalWrite(LATCH, HIGH);
 }
 
+uint64_t readKeyboard() {
+  digitalWrite(LATCH, LOW);
+  digitalWrite(LATCH, HIGH);
+  uint64_t states = shiftIn(DATA, CLOCK, MSBFIRST);
+  return states;
+}
 
 void noteOn(BLECharacteristic *midiDevice, uint8_t note, uint8_t channel) {
   uint8_t channels[3] = { 0x90, 0x91, 0x92 };
@@ -104,4 +93,62 @@ void noteOff(BLECharacteristic *midiDevice, uint8_t note, uint8_t channel) {
   };
   midiDevice->setValue(midiPacket, 5);  // packet, length in bytes
   midiDevice->notify();
+}
+
+void processKeyboardChanges(uint64_t keyboard) {
+  static uint64_t last_keyboard = 0;
+  if (keyboard == last_keyboard) {
+    return;
+  }
+  uint64_t changed = keyboard ^ last_keyboard;
+  last_keyboard = keyboard;
+  for (int i = 0; i < 64; ++i)
+  {
+    if (changed & 1) {
+      handleChange(i, keyboard & 1);
+    }
+    changed >>= 1;
+    keyboard >>= 1;
+  }
+}
+
+void handleChange(int key, int state) {
+  //  Serial.print("#");
+  //  Serial.print(key); // номер кнопки
+  //  Serial.print(" -> ");
+  //  Serial.println(state); // новое состояние
+  uint8_t note = 0;
+  uint8_t channel = 0;
+  if (key < sizeof(rightKeyboard)) {
+    note = rightKeyboard[key];
+    channel = CHANNEL0;
+  }
+  if (state) {
+    Serial.print("noteOn -> ");
+    Serial.println(note);
+    Serial.print("channel -> ");
+    Serial.println(channel);
+    Serial.println("");
+    noteOn(pCharacteristic, note, channel);
+  } else {
+    Serial.print("noteOff -> ");
+    Serial.println(note);
+    Serial.print("channel -> ");
+    Serial.println(channel);
+    Serial.println("");
+    noteOff(pCharacteristic, note, channel);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupKeyboard();
+  setupBle();
+}
+
+void loop() {
+  if (deviceConnected) {
+    uint64_t keyboard = readKeyboard();
+    processKeyboardChanges(keyboard);
+  }
 }
